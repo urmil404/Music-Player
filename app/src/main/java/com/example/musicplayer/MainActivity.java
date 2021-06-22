3c4e2f417b79443e2033f962a7fed0875d2ab94d
urmil89 <urmil8989@gmail.com>
2021-06-22 14:10:27 +0530

    adding new version of damaru

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -1,43 +1,219 @@
 package com.example.musicplayer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView play;
-    MediaPlayer mp;
+    ImageView play, prev, next, imageView;
+    TextView songTitle;
+    SeekBar mSeekBarTime, mSeekBarVol;
+    static MediaPlayer mMediaPlayer;
+    private Runnable runnable;
+    private AudioManager mAudioManager;
+    int currentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        // initializing views
+
         play = findViewById(R.id.play);
-        mp = MediaPlayer.create(MainActivity.this,R.raw.m1);
+        prev = findViewById(R.id.prev);
+        next = findViewById(R.id.next);
+        songTitle = findViewById(R.id.songTitle);
+        imageView = findViewById(R.id.imageView);
+        mSeekBarTime = findViewById(R.id.seekBarTime);
+        mSeekBarVol = findViewById(R.id.seekBarVol);
+
+        // creating an ArrayList to store our songs
+
+        final ArrayList<Integer> songs = new ArrayList<>();
+
+        songs.add(0, R.raw.m1);
+        songs.add(1, R.raw.m2);
+        songs.add(2, R.raw.m3);
+        songs.add(3, R.raw.m4);
+
+        // intializing mediaplayer
+
+        mMediaPlayer = MediaPlayer.create(getApplicationContext(), songs.get(currentIndex));
+
+        // seekbar volume
+
+        int maxV = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int curV = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        mSeekBarVol.setMax(maxV);
+        mSeekBarVol.setProgress(curV);
+
+        mSeekBarVol.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                checking music is playing or not
-                if(mp.isPlaying()){
-                    mp.pause();
+                mSeekBarTime.setMax(mMediaPlayer.getDuration());
+                if (mMediaPlayer != null && mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.pause();
                     play.setImageResource(R.drawable.play_button);
-                }else{
-                    mp.start();
+                } else {
+                    mMediaPlayer.start();
                     play.setImageResource(R.drawable.pause_button);
                 }
+                songNames();
             }
+
         });
 
-        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+        next.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCompletion(MediaPlayer mp) {
-                mp.release();
+            public void onClick(View v) {
+                if (mMediaPlayer != null) {
+                    play.setImageResource(R.drawable.pause_button);
+                }
+
+                if (currentIndex < songs.size() - 1) {
+                    currentIndex++;
+                } else {
+                    currentIndex = 0;
+                }
+
+                if (mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.stop();
+                }
+
+                mMediaPlayer = MediaPlayer.create(getApplicationContext(), songs.get(currentIndex));
+
+                mMediaPlayer.start();
+                songNames();
             }
         });
 
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mMediaPlayer != null) {
+                    play.setImageResource(R.drawable.pause_button);
+                }
+
+                if (currentIndex > 0) {
+                    currentIndex--;
+                } else {
+                    currentIndex = songs.size() - 1;
+                }
+                if (mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.stop();
+                }
+
+                mMediaPlayer = MediaPlayer.create(getApplicationContext(), songs.get(currentIndex));
+                mMediaPlayer.start();
+                songNames();
+            }
+        });
     }
+    private void songNames() {
+        if (currentIndex == 0) {
+            songTitle.setText("Goblin - Korean Song");
+            imageView.setImageResource(R.drawable.headphone);
+        }
+        if (currentIndex == 1) {
+            songTitle.setText("Dil Dhadkanee Do");
+            imageView.setImageResource(R.drawable.headphone);
+        }
+        if (currentIndex == 2) {
+            songTitle.setText("Iktara - Amit Trivedi");
+            imageView.setImageResource(R.drawable.headphone);
+        }
+        if (currentIndex == 3) {
+            songTitle.setText("Start Over - Korean Song");
+            imageView.setImageResource(R.drawable.headphone);
+        }
+        if (currentIndex == 4) {
+            songTitle.setText("Tujhe Kitna");
+            imageView.setImageResource(R.drawable.headphone);
+        }
+        mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mSeekBarTime.setMax(mMediaPlayer.getDuration());
+                mMediaPlayer.start();
+            }
+        });
+
+        mSeekBarTime.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mMediaPlayer.seekTo(progress);
+                    mSeekBarTime.setProgress(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (mMediaPlayer != null) {
+                    try {
+                        if (mMediaPlayer.isPlaying()) {
+                            Message message = new Message();
+                            message.what = mMediaPlayer.getCurrentPosition();
+                            handler.sendMessage(message);
+                            Thread.sleep(1000);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }).start();
+    }
+    @SuppressLint("Handler Leak")
+    Handler handler = new Handler () {
+        @Override
+        public void handleMessage  (Message msg) {
+            mSeekBarTime.setProgress(msg.what);
+        }
+    };
+
 }
